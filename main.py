ee9b33817fffe034c2b6bf8ac3c32574aa1235e2
aricaldoni <>
2021-04-26 21:33:38 +0000

    Debugging

--- main.py
+++ main.py
@@ -26,26 +26,25 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
 game_images = [rock, paper, scissors]
 
-user_choice = int(input("Let´s play this game. What will you chose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(game_images[user_choice - 1])
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
 
 computer_choice = random.randint(0, 2)
 print("Computer chose:")
 print(game_images[computer_choice])
 
 if user_choice >= 3 or user_choice < 0: 
-  print("You typed an invalid number, try again!") 
+  print("You typed an invalid number, you lose!") 
 elif user_choice == 0 and computer_choice == 2:
-  print("Rock breaks scissors, you win!")
+  print("You win!")
 elif computer_choice == 0 and user_choice == 2:
-  print("Rock breaks scissors, you lose")
+  print("You lose")
 elif computer_choice > user_choice:
   print("You lose")
 elif user_choice > computer_choice:
   print("You win!")
 elif computer_choice == user_choice:
-  print("It's a tie, try again")
+  print("It's a draw")
 
